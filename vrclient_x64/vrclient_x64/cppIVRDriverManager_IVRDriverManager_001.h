c778af11b790b4fafc3c6da1c16fd0be5f47f2e1
Jens Peters <jp7677@gmail.com>
2019-09-28 18:42:20 +0200

    Strip everything we don't need anymore.

--- vrclient_x64/vrclient_x64/cppIVRDriverManager_IVRDriverManager_001.h
+++
@@ -1,9 +0,0 @@
-#ifdef __cplusplus
-extern "C" {
-#endif
-extern uint32_t cppIVRDriverManager_IVRDriverManager_001_GetDriverCount(void *);
-extern uint32_t cppIVRDriverManager_IVRDriverManager_001_GetDriverName(void *, DriverId_t, char *, uint32_t);
-extern DriverHandle_t cppIVRDriverManager_IVRDriverManager_001_GetDriverHandle(void *, const char *);
-#ifdef __cplusplus
-}
-#endif
--- vrclient_x64/vrclient_x64/vrclient_x64.spec
+++
@@ -1,4 +0,0 @@
-# Generated from vrclient.dll by winedump
-
-1 stub HmdSystemFactory
-2 stdcall VRClientCoreFactory(ptr ptr)
